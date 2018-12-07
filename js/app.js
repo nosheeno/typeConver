a32c4e7042b4fe95568e637e9e9a49a1cdb47d5c
nosheeno <nosheen@uconnect.pk>
2018-12-07 16:13:17 +0500

    adding the test results of type conversion

---
+++ js/app.js
@@ -0,0 +1,38 @@
+var name;   //here NAME IS UNDEFINED
+name = "nosheen"; //string
+const age = 23 ;//string
+console.log("Name " + name + " Age " + age);
+console.log(typeof(name), typeof(age));
+
+let x = 2; // number
+y = 3;
+z = 2;
+console.log(typeof(x==y)); //boolean
+console.log(x==z);
+
+let a = "", b = 1, c = 0;
+console.log(a+b+c);
+console.log(a-1+0);
+
+let d = true, e = false;
+console.log(d+e);
+
+let f = 6, g = "3", h = "2";
+console.log(f/g);
+console.log(h*g);
+
+let i =4, j=5, k ="px";
+console.log(i+j+k);
+
+let l ="4", m =2, n="4px";
+console.log(l-m);
+console.log(n-m);
+
+let o = 7, p =0, q="-9/n", r=5;
+console.log(o/p);
+console.log(q+r);
+console.log(q-r);
+
+let s= null, t =1, u=undefined;
+console.log(s+t);
+console.log(u+t);
